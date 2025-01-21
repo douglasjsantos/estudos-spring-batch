2c0852bdb4a7dc080862d6a8c819f3c220d9b9a6
Douglas Santos <douglas_j.santos@hotmail.com>
2025-01-21 17:52:07 -0300

    criado o job e projeto

--- src/main/java/com/example/demo/step/PrintHelloStepConfig.java
+++ src/main/java/com/example/demo/step/PrintHelloStepConfig.java
@@ -7,12 +7,15 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class PrintHelloStepConfig {
+
+
 
     @Bean
     public Step printHelloStep(JobRepository jobRepository, PlatformTransactionManager transactionManager, PrintHelloTasklet printHelloTasklet){
--- src/main/java/com/example/demo/tasklet/PrintHelloTasklet.java
+++ src/main/java/com/example/demo/tasklet/PrintHelloTasklet.java
@@ -5,11 +5,13 @@ import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 @StepScope
 public class PrintHelloTasklet implements Tasklet {
+
     @Override
     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
         System.out.println("Olá Mundo");
